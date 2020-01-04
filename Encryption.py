f4d26f19a2f7675b227dd0a696b41c347f4a6680
unknown <icampus_bnu@163.com>
2020-01-04 16:59:56 +0800

    compile files to SO file

--- Encryption.py
+++ Encryption.py
@@ -5,4 +5,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 # Example call: sudo python Encryption.py build_ext
-setup(ext_modules = cythonize(["AnalyzeGrade.py","CalcCourseMetric.py","CalcMetric.py","ClassAPI.py","CommonUtil.py","Config.py","EstimateMental.py","MetricUtil.py","PostMetric.py","Preprocessor.py"]))
+setup(ext_modules = cythonize(["AnalyzeGrade.py","CalcCourseMetric.py","CalcMetric.py","ClassAPI.py","CommonUtil.py","Config.py","EstimateMental.py","MetricUtil.py","PostMetric.py","Preprocessor.py", "CalcClassMetric.py", "CalcTeacherMetric.py", "CalcTeaCourseMetric.py"]))
